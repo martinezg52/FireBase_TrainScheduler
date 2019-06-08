fa3f387d77c4b17ac3da3765a65885cc157f0bfb
geo_ep <geoffrey.xwin@gmail.com>
2019-06-08 07:16:31 -0700

    working on firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+//Initialize Firebase
+
+$(document).ready(function() {
+
+var config = {
+    apiKey: "AIzaSyBemrO8C4qMXAEqaHdWB9D9H8pa7ewMTrE",
+    authDomain: "trainscheduler-18d12.firebaseapp.com",
+    databaseURL: "https://trainscheduler-18d12.firebaseio.com",
+    projectId: "trainscheduler-18d12",
+    storageBucket: "gs://trainscheduler-18d12.appspot.com",
+};
+
+})
